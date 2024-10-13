4884edfe5cd3f789ab5f304093ff5f75f57e0ac0
Matheus <133925124+limematheuz@users.noreply.github.com>
2024-10-13 21:25:20 +0000

    "add new spinner component"

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,14 +1,5 @@
 import "./Spinner.css";
 
 export default function Spinner() {
-  return (
-    <div className="sk-chase">
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-    </div>
-  );
+  return <span className="loader"></span>;
 }
